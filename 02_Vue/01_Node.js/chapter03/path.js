04cab41845deda700f39490da9f54a106dce4b2f
roa <roa5108@gmail.com>
2025-03-13 17:44:56 +0900

    주석

--- 02_Vue/01_Node.js/chapter03/path.js
+++ 02_Vue/01_Node.js/chapter03/path.js
@@ -1,23 +1,28 @@
 const path = require('path');
 
+//join
 const fullPath = path.join('some', 'work', 'ex.txt');
 console.log(fullPath);
 
 console.log(`파일 절대 경로: ${__filename}`);
 
+//dirname
 const dir = path.dirname(__filename);
 console.log(`경로만: ${dir}`);
 
+//basename
 const fn = path.basename(__filename);
 const fn2 = path.basename(__filename, '.js');
 
 console.log(`파일 이름: ${fn}`);
 console.log(`파일 이름(확장자 제외): ${fn2}`);
 
+//extname
 const ext = path.extname(__filename);
 console.log(`파일 확장자: ${ext}`);
 console.log(path.basename(__filename, ext));
 
+//parse
 const parsedPath = path.parse(__filename);
 console.log(parsedPath);
 
