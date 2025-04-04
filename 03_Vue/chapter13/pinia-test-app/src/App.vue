e3a39bf0261b92006c0aa8851c1145d469c71aa4
roa <roa5108@gmail.com>
2025-04-04 17:14:40 +0900

    0404

---
+++ 03_Vue/chapter13/pinia-test-app/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed } from 'vue';
+import { useCounterStore } from '@/stores/counter.js';
+
+const store = useCounterStore();
+const { increment } = store;
+const count = computed(() => store.count);
+
+increment();
+console.log(count);
+</script>
+
+<template>
+  {{ store.count }}/{{ store.doubleCount }}<br />
+  {{ count }}
+</template>
